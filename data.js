b292df406aad77ee53394d94d2f819e9f63a914c
Leslie Castellanos <lesliekate@gmail.com>
2017-03-07 08:18:20 -0800

    sample data

---
+++ data.js
@@ -0,0 +1,130 @@
+var cats = {
+    "Street Touring": {
+        STF: 0.8, 
+        STU: 0.831, 
+        STP: 0.82, 
+        STR: 0.83, 
+        STS: 0.818, 
+        STX: 0.822
+      }, 
+      Modified: {
+        EM: 0.905, 
+        DM: 0.906, 
+        CM: 0.901, 
+        FSAE: 0.966, 
+        BM: 0.956, 
+        AM: 1, 
+        KM: 0.939, 
+        FM: 0.916
+      }, 
+      Prepared: {
+        FP: 0.873, 
+        HCR: 0.825, 
+        BP: 0.869, 
+        XP: 0.892, 
+        CP: 0.854, 
+        EP: 0.859, 
+        DP: 0.865
+      }, 
+      "Street Modified": {
+        SSM: 0.875, 
+        SMF: 0.848, 
+        SM: 0.861
+      }, 
+      Street: {
+        HCS: 0.809, 
+        FS: 0.804, 
+        GS: 0.793, 
+        SS: 0.826, 
+        HS: 0.786, 
+        SSR: 0.847, 
+        AS: 0.819, 
+        BS: 0.813, 
+        CS: 0.81, 
+        DS: 0.801, 
+        ES: 0.794
+      }, 
+      CAM: {
+        "CAM-C": 0.823, 
+        "CAM-S": 0.838, 
+        "CAM-T": 0.817
+      }, 
+        "Street Prepared": {
+          ASP: 0.856, 
+          ESP: 0.837, 
+          BSP: 0.853, 
+          SSP: 0.862, 
+          DSP: 0.842, 
+          FSP: 0.829, 
+          CSP: 0.86
+      }, 
+      Junior: {
+        JA: 0.864, 
+        JB: 0.834, 
+        JC: 0.726
+      }
+    }
+
+const prosoloCats = {
+        Street: {
+          SS: 0.815,
+          AS: 0.813,
+          BS: 0.807,
+          CS: 0.792,
+          DS: 0.788,
+          ES: 0.782,
+          FS: 0.790,
+          GS: 0.779,
+          HS: 0.768,
+          SSR: 0.855,
+        },
+        'Street Touring': {
+          STF: 0.789,
+          STS: 0.799,
+          STX: 0.809,
+          STR: 0.819,
+          STU: 0.823,
+          STP: 0.811,
+        },
+        'Street Prepared': {
+          SSP: 0.871,
+          ASP: 0.868,
+          BSP: 0.858,
+          CSP: 0.861,
+          DSP: 0.848,
+          ESP: 0.845,
+          FSP: 0.818,
+        },
+        CAM: {
+          'CAM-C': 0.816,
+          'CAM-T': 0.805,
+          'CAM-S': 0.822,
+        },
+        Prepared: {
+          XP: 0.904,
+          CP: 0.856,
+          DP: 0.870,
+          EP: 0.863,
+          FP: 0.873,
+        },
+        'Street Mod': {
+          SMF: 0.859,
+          SM: 0.872,
+          SSM: 0.881,
+        },
+        Mod: {
+          AM: 1,
+          BM: 0.955,
+          CM: 0.896,
+          DM: 0.920,
+          EM: 0.922,
+          FM: 0.924,
+          FSAE: 0.958,
+          KM: 0.934,
+        },
+        Junior: {
+          JA: 0.821,
+          JB: 0.781,
+          JC: 0.717,
+        }
+    }
